df5f4ef123526ac71052936ada256015be673951
ericw <ericw@UncleRicco.san.rr.com>
2025-01-29 17:58:19 -0800

    added a simple addition and implemented add()

--- src/demo1/DemoTest5.java
+++ src/demo1/DemoTest5.java
@@ -2,6 +2,19 @@ package demo1;
 
 public class DemoTest5 {
 	public static void main(String[] args) {
-		System.out.println("Java");
+		System.out.println("Uncle Ricco Batch 13: GIT Lecture");
+		
+		
+		int a = 10;
+		int b = 20;
+		
+		int result = a + b;
+		
+		System.out.println(result);
 }
+	
+	public void add(int a, int b) {
+		System.out.println(a+ b);
+	}
+	
 }
